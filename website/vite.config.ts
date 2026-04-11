03f4b445cdd7d6ca17816129cc0a4174469cf185
Omar Salazar <omarsalazar1025@gmail.com>
2026-04-11 13:43:10 -0500

    Simplify vite config for CI compatibility

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,10 +1,8 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  root: path.resolve(__dirname),
   base: command === 'build' ? '/Omar102598/' : '/',
   server: {
     cors: true,
